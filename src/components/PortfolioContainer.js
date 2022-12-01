39b1e2034bce6d9007e001713c729f37fb7332c5
Emma Velazquez <emmasiren24@gmail.com>
2022-12-01 02:04:09 -0800

    Added navtab layout, updated portfoliocontainer layout. Testing on locally now.

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
-import projects from './pages/ProjectList'
+import projects from './pages/ProjectList';
+import Resume from './pages/Resume';
+
 
 export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  const renderPage = () => {
+    if(currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if(currentPage === 'Portfolio') {
+      return <Portfolio projects={projects}/>
+    }
+    if(currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div>
-      <Portfolio projects={projects}/>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
   );
 }
